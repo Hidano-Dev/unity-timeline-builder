9ed3dda9787c3b4e3112c1df6e15e8297c69ec25
Hidano <n.hidano@hidano-dev.com>
2026-08-21 01:03:16 +0900

    Add Test_3b8f368056d8411fb2f8a9c9611622e3.meta, Test_e9632e0798604d77b438435b115ca77c.meta; Update ExternalResourceIntegrationTests.cs, external-resource-integration.csv

--- UnityTimelineBuilder/Packages/com.hidano.unity-timeline-builder/Tests/Editor/ExternalResourceIntegrationTests.cs
+++ UnityTimelineBuilder/Packages/com.hidano.unity-timeline-builder/Tests/Editor/ExternalResourceIntegrationTests.cs
@@ -70,7 +70,7 @@ namespace Hidano.UnityTimelineBuilder.Editor.Tests
             Assert.That(((AudioPlayableAsset)audioTrack.GetClips().First().asset).clip, Is.Not.Null);
             Assert.That(((AudioPlayableAsset)audioTrack.GetClips().Last().asset).clip, Is.Not.Null);
             var animationClip = timeline.GetOutputTracks().OfType<AnimationTrack>().Single().GetClips().Single();
-            Assert.That(((AnimationPlayableAsset)animationClip.asset).clip.name, Is.EqualTo("Walk"));
+            Assert.That(((AnimationPlayableAsset)animationClip.asset).clip.name, Is.EqualTo("Anim|Walk"));
         }
 
         [Test]
